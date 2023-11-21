4176ac519f1fc0ed2da4bf13c0d735b9a2826e2a
mario081 <ffa35922@gmail.com>
2023-11-21 09:37:15 -0300

    Mandar dados para o banco

--- BackEnd/login.php
+++ BackEnd/login.php
@@ -7,5 +7,20 @@
             die("Erro ao conectar ao banco de dados" . mysqli_connect_error());
         }
 
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+
+        $sql = "SELECT * FROM cadastro WHERE email = '$email' AND senha = '$senha'";
+        $resultado = $conn->query($sql);
+
+            if ($resultado->num_row > 0) {
+                echo "Login bem-sucedido!"
+            } else {
+                echo "Email ou senha incorretas!"
+
+            }
+
+            $conn->close();
+
 
 ?>
