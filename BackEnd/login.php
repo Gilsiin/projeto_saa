0b23b3587f3a2b6152ab13f68ecc71ae0e32a248
mario081 <ffa35922@gmail.com>
2023-11-21 09:20:35 -0300

    Update login.php

--- BackEnd/login.php
+++ BackEnd/login.php
@@ -1,7 +1,7 @@
 <?php
 //login
 
-    $conn = mysqli_connect($Host, $User, $Pass, $db);
+    $conn = mysqli_connect("localhost", "root", "", "login");
 
         if (!$conn) {
             die("Erro ao conectar ao banco de dados" . mysqli_connect_error());
