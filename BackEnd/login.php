df9cd5da4ea91c8178e7e6a946b546a87f712db0
mario081 <ffa35922@gmail.com>
2023-11-24 12:04:20 -0300

    teste da tela de login

--- BackEnd/login.php
+++ BackEnd/login.php
@@ -1,35 +1,42 @@
 <?php
-//login
 
-    $conn = mysqli_connect("localhost", "root", "", "login");
+  include ('config.php');
 
-        if (!$conn) {
-            die("Erro ao conectar ao banco de dados" . mysqli_connect_error());
-        }
+    
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['email']) || isset($_POST['senha'])) {
 
-        $email = $_POST["email"];
-        $senha = $_POST["senha"];
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu email";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
 
-        if ($email == "Admin" && $senha == "admin") {
-            header("Location: /projeto_saa/pag03.html");
-            exit();
-        } else {
-            echo "Credenciais inválidas. Tente novamente.";
-        }
+        $email = $con->real_escape_string($_POST['email']); 
+        $senha = $con->real_escape_string($_POST['senha']); 
+       
+        $sql_code = "SELECT * FROM login WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $con->query($sql_code) or die("Falha na execução do codigo SQL; " . $con->error);
 
-        $sql = "SELECT * FROM cadastro WHERE email = '$email' AND senha = '$senha'";
-        $resultado = $conn->query($sql);
+        $quantidade = $sql_query->num_rows;
 
-            if ($resultado->num_rows > 0) {
-                echo "Login bem-sucedido!";
-            } else {
-                echo "Email ou senha incorretas!";
+        if($quantidade == 1) {
 
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)){
+                session_start();
             }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: pag04.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos ";
         }
-            $conn->close();
 
-
+    }
+}
 ?>
